f50a56bdd67f74936be803e0c82f2959c75dd698
russhail <russhailwood@gmail.com>
2023-01-25 09:14:43 +0000

    Update tables to sources

--- coffee_shop_russell/models/customers.sql
+++ coffee_shop_russell/models/customers.sql
@@ -4,8 +4,8 @@ SELECT
   customers.email,
   MIN(orders.created_at) AS first_order_at,
   SUM(CASE WHEN orders.created_at IS NOT NULL THEN 1 ELSE 0 END) as number_of_orders
-FROM `analytics-engineers-club.coffee_shop.customers` customers
-LEFT JOIN `analytics-engineers-club.coffee_shop.orders` orders
+FROM {{ source('coffee_shop', 'customers') }}
+LEFT JOIN {{ source('coffee_shop', 'orders') }}
 ON customers.id = orders.customer_id
 GROUP BY 1,2,3
 order by first_order_at
