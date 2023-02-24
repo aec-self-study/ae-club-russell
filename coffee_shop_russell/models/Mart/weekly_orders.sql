900cbe63aa363e400f47bb3cb807a70250caebbc
russhail <russhailwood@gmail.com>
2023-02-24 13:05:50 +0000

    Add aliases

--- coffee_shop_russell/models/Mart/weekly_orders.sql
+++ coffee_shop_russell/models/Mart/weekly_orders.sql
@@ -1,6 +1,6 @@
 SELECT
-EXTRACT(YEAR FROM created_at),
-EXTRACT(WEEK FROM created_at)
-, SUM(ORDER_AMOUNT)
+EXTRACT(YEAR FROM created_at) as order_year,
+EXTRACT(WEEK FROM created_at) as order_week
+, SUM(ORDER_AMOUNT) as revenue_total
 FROM {{ ref('stg_orders') }}
 GROUP BY 1,2
