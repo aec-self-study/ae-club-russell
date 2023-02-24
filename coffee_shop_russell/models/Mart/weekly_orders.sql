3df5eea2c08b0afbe338ecc989867e615c9b2853
russhail <russhailwood@gmail.com>
2023-02-24 13:57:13 +0000

    Add weekly_orders by customer type returning or new

--- coffee_shop_russell/models/Mart/weekly_orders.sql
+++ coffee_shop_russell/models/Mart/weekly_orders.sql
@@ -1,6 +1,6 @@
 SELECT
 EXTRACT(YEAR FROM created_at) as order_year,
 EXTRACT(WEEK FROM created_at) as order_week
-, SUM(ORDER_AMOUNT) as revenue_total
+, SUM(ORDER_AMOUNT) as revenue_totaldb
 FROM {{ ref('stg_orders') }}
 GROUP BY 1,2
